0176d1c7d172459769fa218c751b11665af82d0a
NytroxDev <17164.fichier47@gmail.com>
2026-02-13 15:34:49 +0100

    Revert "test"
    This reverts commit 4c8b27cce2ce998f5d3370b5197533b1e0dcf570.

--- core/color_converter.py
+++
@@ -1,9 +0,0 @@
-def rgb_to_hex(rgb):
-    r, g, b = rgb
-    return f"#{r:02X}{g:02X}{b:02X}"
-
-
-def hex_to_rgb(hex_color):
-    hex_color = hex_color.lstrip("#")
-
-    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
